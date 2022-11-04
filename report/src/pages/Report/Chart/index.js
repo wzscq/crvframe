f214089eaa4adb129b2e34da96d3ad7a5d0b2de2
wangzhongshu <wzscq@126.com>
2022-11-04 16:22:33 +0800

    commit changes

--- report/src/pages/Report/Chart/index.js
+++ report/src/pages/Report/Chart/index.js
@@ -82,7 +82,7 @@ export default function Chart({controlConf,reportID,sendMessageToParent,framePar
 
     return (
         <div style={wrapperStyle}>
-            <div ref={refChart} style={{width:'100%',height:'100%'}}>{JSON.stringify(option)}</div>
+            <div ref={refChart} style={{width:'100%',height:'100%'}} />
             <div ref={ref} style={{width:'100%',height:'100%'}}>{}</div>
         </div>
     );
