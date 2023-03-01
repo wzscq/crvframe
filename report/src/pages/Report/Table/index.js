0319cf13cb783f4fc6abf94cef98514cafa9a829
wangzhongshu <wzscq@126.com>
2023-03-01 13:28:22 +0800

    commit changes

--- report/src/pages/Report/Table/index.js
+++ report/src/pages/Report/Table/index.js
@@ -1,16 +1,15 @@
-import { useEffect,useMemo } from "react";
-import { Timeline as TimelineCtl } from 'antd';
+import { useEffect } from "react";
+import { Table as AntdTable } from 'antd';
 import { useDispatch, useSelector } from "react-redux";
 
 import {setDataLoaded} from '../../../redux/reportSlice';
 import {FRAME_MESSAGE_TYPE} from '../../../utils/constant';
-import Title from './Title';
 
 import './index.css';
 
-export default function DateLine({controlConf,reportID,sendMessageToParent,frameParams}){
+export default function Table({controlConf,reportID,sendMessageToParent,frameParams}){
   const {id,option,minHeight,row,col,colSpan,rowSpan}=controlConf;
-  const {title,label,content}=option;
+  const {footer,title,columns,pagination}=option;
   const filterData=useSelector(state=>state.data.updated[Object.keys(state.data.updated)[0]]);
   const data=useSelector(state=>state.report.chart[id]);
   const dispatch=useDispatch();
@@ -35,44 +34,6 @@ export default function DateLine({controlConf,reportID,sendMessageToParent,frame
     }
   },[data,id,frameParams,reportID,filterData,sendMessageToParent,dispatch]);
 
-  const labelFunc=useMemo(()=>{
-    const funStr='"use strict";'+
-                 'return (function(data){ '+
-                    'try {'+
-                      label+
-                    '} catch(e) {'+
-                    '   console.error(e);'+
-                    '   return undefined;'+
-                    '}'+
-                 '})';
-    return Function(funStr)();
-  },[label]);
-
-  const contentFunc=useMemo(()=>{
-    const funStr='"use strict";'+
-                 'return (function(data){ '+
-                    'try {'+
-                    content+
-                    '} catch(e) {'+
-                    '   console.error(e);'+
-                    '   return undefined;'+
-                    '}'+
-                 '})';
-    return Function(funStr)();
-  },[content]);
-
-  const items=useMemo(()=>{
-    if(data?.loaded===true){
-        //根据配置和数据生成系列参数
-        if(data.list&&data.list.length>0){
-            return data.list.map(dataItem=>{
-              return (<TimelineCtl.Item label={labelFunc(dataItem)}>{contentFunc(dataItem)}</TimelineCtl.Item>);
-            });
-        }
-    }
-    return null;
-  },[data,labelFunc,contentFunc]);
-  
   const wrapperStyle={
     gridColumnStart:col,
     gridColumnEnd:col+colSpan,
@@ -84,12 +45,43 @@ export default function DateLine({controlConf,reportID,sendMessageToParent,frame
     height:'100%'
   }
 
+  const titleFunc=()=>{
+    const funStr='"use strict";'+
+                 'return (function(currentPageData){ '+
+                    'try {'+
+                    title+
+                    '} catch(e) {'+
+                    '   console.error(e);'+
+                    '   return undefined;'+
+                    '}'+
+                 '})';
+    return Function(funStr)();
+  };
+
+  const footerFunc=()=>{
+    const funStr='"use strict";'+
+                 'return (function(currentPageData){ '+
+                    'try {'+
+                    footer+
+                    '} catch(e) {'+
+                    '   console.error(e);'+
+                    '   return undefined;'+
+                    '}'+
+                 '})';
+    return Function(funStr)();
+  };
+
   return (
-    <div style={wrapperStyle} className='report-dateline'>
-      <Title text={title}/>
-      <TimelineCtl mode={'left'} >
-        {items}
-      </TimelineCtl>
+    <div style={wrapperStyle} className="report-table">
+      <AntdTable 
+        title={titleFunc()}
+        size='small' 
+        columns={columns} 
+        dataSource={data?.list} 
+        bordered
+        pagination={pagination}
+        footer={footerFunc()}
+      />
     </div>
   );
 }
--- report/src/pages/Report/controls.js
+++ report/src/pages/Report/controls.js
@@ -1,17 +1,20 @@
 import Chart from './Chart';
 import DateLine from './DateLine';
+import Table from './Table';
 
 /**
  * 以下为控件类型枚举常量定义
  */
  export const CONTROL_TYPE={
-  EChart:"EChart",   //文本录入框
-  DATELINE:'DateLine'  //多行文本编辑框
+  ECHART:"EChart",   //文本录入框
+  DATELINE:'DateLine',  //多行文本编辑框
+  TABLE:"Table"   //表格
 }
 
 const controls={
-  [CONTROL_TYPE.EChart]:Chart,
-  [CONTROL_TYPE.DATELINE]:DateLine
+  [CONTROL_TYPE.ECHART]:Chart,
+  [CONTROL_TYPE.DATELINE]:DateLine,
+  [CONTROL_TYPE.TABLE]:Table
 }
 
 export const getControl=(control,frameParams,reportID,sendMessageToParent)=>{
