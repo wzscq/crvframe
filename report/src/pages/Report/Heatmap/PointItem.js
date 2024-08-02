6931c1d12988314ceaf1bf03c3dab611b50a4956
wangzhongshu <wzscq@126.com>
2024-08-02 15:00:05 +0800

    commit changes

--- report/src/pages/Report/Heatmap/PointItem.js
+++ report/src/pages/Report/Heatmap/PointItem.js
@@ -2,8 +2,17 @@
 import { Tooltip } from 'antd';
 
 export default function PointItem({item}){
+    let open=undefined;
+    if(item.open==1){
+        open=true;
+    }
+
+    if(item.open==0){
+        open=false;
+    }
+
     return (
-        <Tooltip title={item.tip}>
+        <Tooltip title={item.tip} open={open}>
             <div className='point-item' style={{left:item.x+'px',top:item.y+'px'}}/>
         </Tooltip>
     )
